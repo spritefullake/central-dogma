4620bf2f3400350f79115a718e9cb814b6e63d6f
spritefullake <42ahmad@gmail.com>
2020-04-06 18:48:42 -0400

    adjusted server port soruce

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(webpackDevMiddleware(compiler, {
   publicPath: config.output.publicPath,
 }));
 
-// Serve the files on port 3000.
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!\n');
+let {port} = config.devServer;
+app.listen(port, function () {
+  console.log(`Listening on port ${port}\nGo to localhost:${port}`);
 });
