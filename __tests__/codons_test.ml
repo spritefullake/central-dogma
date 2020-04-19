07ec819db75dfb325a8037f031dabf7487514a81
spritefullake <42ahmad@gmail.com>
2020-04-19 03:01:54 -0400

    updating tests to reflect changes

--- __tests__/codons_test.ml
+++ __tests__/codons_test.ml
@@ -1,18 +1,26 @@
 open Jest
 open Expect
 open Codons
+open LoadCodons
 
-let () = ()
-(*
+(* Mocking the LoadCodons load *)
+let loadMock = (fun () ->  
+    Node.Path.normalize "Codons.tsv"
+    |> Node.Fs.readFileAsUtf8Sync
+    |> processRaw3Code
+)
+let () =
+
 describe "Matching codons with Amino Acids" (fun () ->
 
     test "Start codon is AUG -> Methionine" (fun () -> 
-        let result = ["A";"U";"G"] |> match_codon in
-        expect result |> toEqual (Some "M")
+        let table = loadMock() in
+        let matched = match_codon `Three ["A";"U";"G"] table in
+        expect matched |> toEqual (Some "Met")
     );
+    (*
     test "UUU is Phenylalanine" (fun () -> 
         let result = ["U";"U";"U"] |> match_codon in
         expect result |> toEqual (Some "F")
-    );
+    );*)
 );
-*)
--- src/Codons.ml
+++ src/Codons.ml
@@ -14,15 +14,17 @@ let string_to_effect = function
 | "*" -> Stop
 | _ -> Nothing
 type table = string t array
+
 let display_codons input = 
     input 
     |> Array.of_list
     |> Array.map(Array.of_list)
     |> Array.map (fun x -> x |> Array.to_list |> String.concat "")
+
 let make_codons input = 
     input
-    |> Array.to_list
     |> chunk_list 3
+(* Match a codon to an amino acid on the codon table *)
 let match_codon code_type input source =
   Array.fold_left (fun acc {codon; code1; code3} -> 
     match acc with 
@@ -30,14 +32,16 @@ let match_codon code_type input source =
       if input = codon 
       then match code_type with
       | `One -> Some code1
-      | _ -> Some code3
+      | `Three | _ -> Some code3
       else None
     | _ -> acc
   ) None source
+
 let match_codons ~letter_code ~input ~source = 
   input 
   |> make_codons 
   |> List.map (fun x -> match_codon letter_code x source)
+
 let display_matches input =
   List.map (function 
     | Some codon -> codon
