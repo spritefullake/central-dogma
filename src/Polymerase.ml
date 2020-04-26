fe6dbab03a7ca3f54232703088c6f4a3dc00c90b
spritefullake <42ahmad@gmail.com>
2020-04-26 00:21:16 -0400

    Scaffolding a modular version of polymerases using functors

--- src/Polymerase.ml
+++ src/Polymerase.ml
@@ -16,6 +16,7 @@ let transcribe_pairings = [
   `U, `A; `A, `T;  `G, `C 
 ]
 
+
 let walk_assoc base = (fun acc (first, second) ->
   match acc with | Some _ as b -> b
   | None -> 
@@ -46,3 +47,64 @@ let rna_polymerase base =
 let reverse_transcriptase base =
   (* Reverse inverts the priority so A -> T instead of A -> U*)
   make_polymerase (List.rev transcribe_pairings) base
+
+type bases = [ `A | `T | `G | `C | `U ]
+
+type _ nucleic_acid =
+| D : [`A | `T | `G | `C] -> [`A | `T | `G | `C] nucleic_acid
+| R : [`A | `U | `G | `C] -> [`A | `U | `G | `C] nucleic_acid
+| None
+
+module type System = sig
+  type t 
+  val (>>=) : t nucleic_acid -> (bases -> 'a nucleic_acid) -> 'a nucleic_acid
+  val complement : bases -> t nucleic_acid
+end
+module type Transcribes = sig 
+  include System
+  val replicate : t nucleic_acid -> t nucleic_acid 
+  val transcribe : (module System with type t = 'a) -> t nucleic_acid -> 'a nucleic_acid
+end
+
+module MakePolymerase(M : System) : Transcribes with type t = M.t = struct
+  include M 
+  let replicate nucleotide = 
+    nucleotide >>= complement
+  let transcribe (type a) (module Into : System with type t = a) nucleotide =
+    nucleotide >>= Into.complement
+end
+
+module DnaSystem = struct
+  type t = [`A | `T | `G | `C]
+  let complement = function
+  | `A -> D `T
+  | `G -> D `C
+  | `T -> D `A
+  | `C -> D `G
+  | `U -> D `A 
+  | _ -> None
+  let (>>=) nucleotide f = 
+    match nucleotide with
+    | D base -> f (base :> bases)
+    | _ -> None
+end
+module RnaSystem = struct
+  type t = [`A | `U | `G | `C]
+  let complement = function
+  | `A -> R `U
+  | `G -> R `C
+  | `T -> R `A
+  | `C -> R `G
+  | `U -> R `A 
+  | _ -> None
+  let (>>=) nucleotide f = 
+    match nucleotide with
+    | R base -> f (base :> bases)
+    | _ -> None
+end
+module DNA = MakePolymerase(DnaSystem)
+module RNA = MakePolymerase(RnaSystem)
+let rna = (module RNA : System with type t = RNA.t)
+
+
+let e = DNA.replicate (D `A)
