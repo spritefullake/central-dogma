09b03cacacad76ca8f69ba3408f645fef8dac627
spritefullake <42ahmad@gmail.com>
2020-05-06 01:26:42 -0400

    refactored enzyme module interfaces

--- src/Enzyme.ml
+++ src/Enzyme.ml
@@ -4,6 +4,7 @@ module type System = sig
   type rna_bases = [ `A | `U | `G | `C ]
   val complementD: [< dna_bases | rna_bases] -> [> dna_bases]
   val complementR: [< dna_bases | rna_bases] -> [> rna_bases]
+  val to_string: [< dna_bases | rna_bases] -> string
 end
 module Standard : System = struct
   type dna_bases = [`A | `T | `G | `C] 
@@ -16,16 +17,11 @@ module Standard : System = struct
   | `A -> `U | `U -> `A 
   | `G -> `C | `C -> `G 
   | `T -> `A 
+  let to_string = function 
+  | `A -> "A" | `T -> "T" | `U -> "U" 
+  | `G -> "G" | `C -> "C"
 end
 module type Mappable = sig 
   type 'a t 
   val map: ('a -> 'b) -> 'a t -> 'b t
 end
-module type Enzyme = sig 
-  include System
-  include Mappable
-  type 'a strand = 'a t
-  type backbone = D of dna_bases strand  | R of rna_bases strand
-  val replicate: backbone -> backbone 
-  val transcribe: backbone -> backbone
-end 
