14e261fdd741cb278c8a5c5878f94bd23d1b3c35
spritefullake <42ahmad@gmail.com>
2020-05-06 01:26:23 -0400

    cleaned up bases.ml

--- src/Bases.ml
+++ src/Bases.ml
@@ -6,15 +6,16 @@ let to_base = function
   | "G" -> Some `G
   | _ -> None
 let base_to_string = function
-  | `A -> Some "A"
-  | `U -> Some "U"
-  | `C -> Some "C"
-  | `G -> Some "G"
-  | `T -> Some "T"
-  | _ -> None
-let option_of_string = function
-| Some x -> x
-| _ -> ""
+  | `A -> "A"
+  | `U -> "U"
+  | `C -> "C"
+  | `G -> "G"
+  | `T -> "T"
+  | _ -> ""
+let option_to_string = function
+| Some x -> x 
+| None -> ""
+let option_of_string = option_to_string
 let bind base f =
   match base with 
   | Some b -> f b 
@@ -27,6 +28,6 @@ let map base f =
 let parse_strand f = Array.map (fun base -> base >>= f)
 let strand_to_string strand = 
   strand 
-  |> Array.map (fun base -> base >>= base_to_string |> option_of_string)
+  |> Array.map (fun base -> base >>= (fun x -> Some (x |> base_to_string)) |> option_to_string)
   |> Array.to_list |> String.concat ""
 let parse_then_string strand  f = parse_strand f strand |> strand_to_string
