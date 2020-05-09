8cf1ca50528df146baeb8063361dbc188e94272b
spritefullake <42ahmad@gmail.com>
2020-05-09 01:42:57 -0400

    acceptable alternative implementation of statically determinate genetics

--- src/Enzyme.ml
+++ src/Enzyme.ml
@@ -1,25 +1,50 @@
 
 module type System = sig 
-  type dna_bases = [ `A | `T | `G | `C ] 
-  type rna_bases = [ `A | `U | `G | `C ]
-  val complementD: [< dna_bases | rna_bases] -> [> dna_bases]
-  val complementR: [< dna_bases | rna_bases] -> [> rna_bases]
-  val to_string: [< dna_bases | rna_bases] -> string
+  type dna = [`A | `T | `G | `C] 
+  type rna = [`A | `U | `G | `C] 
+  type bases = private [> rna | dna]
+  type _ backbone = 
+  | D: dna -> dna backbone 
+  | R: rna -> rna backbone
+  | None  
+  val toDNA: bases -> dna backbone 
+  val toRNA: bases -> rna backbone
+  val replicate: 'a backbone -> 'a backbone 
+  val transcribe: dna backbone -> rna backbone 
+  val reverse_transcribe: rna backbone -> dna backbone
 end
 module Standard : System = struct
-  type dna_bases = [`A | `T | `G | `C] 
-  type rna_bases = [`A | `U | `G | `C]
-  let complementD = function 
-  | `A -> `T | `T -> `A 
-  | `G -> `C | `C -> `G 
-  | `U -> `A 
-  let complementR = function 
-  | `A -> `U | `U -> `A 
-  | `G -> `C | `C -> `G 
-  | `T -> `A 
-  let to_string = function 
-  | `A -> "A" | `T -> "T" | `U -> "U" 
-  | `G -> "G" | `C -> "C"
+  type dna = [`A | `T | `G | `C] 
+  type rna = [`A | `U | `G | `C] 
+  type bases = private [> rna | dna]
+  type _ backbone = 
+  | D: dna -> dna backbone 
+  | R: rna -> rna backbone
+  | None 
+
+  let complementDNA = function 
+  | `A -> D `T | `T -> D `A 
+  | `G -> D `C | `C -> D `G 
+  | `U -> D `A 
+  | _ -> None
+  let complementRNA = function 
+  | `U -> R `A | `A -> R `U 
+  | `G -> R `C | `C -> R `G 
+  | `T -> R `A 
+  | _ -> None
+  let toDNA = function #dna as base -> D base | _ -> None
+  let toRNA = function #rna as base -> R base | _ -> None
+  
+  let replicate : type a . a backbone -> a backbone = function 
+  | D base -> complementDNA (base :> bases) 
+  | R base -> complementRNA (base :> bases)
+  | None -> None
+  let transcribe = function
+  | D base -> complementRNA (base :> bases)
+  | None -> None
+  let reverse_transcribe = function 
+  | R base -> complementDNA (base :> bases) 
+  | None -> None 
 end
 module type Mappable = sig 
   type 'a t 
--- src/Polymerase.ml
+++ src/Polymerase.ml
@@ -2,58 +2,16 @@ module Options : Enzyme.Mappable with type 'a t = 'a option array = struct
   type 'a t = 'a option array 
   let map f = Array.map (function | Some x -> Some (f x) | None -> None)
 end
-
-module ConstructGrammar (S : Enzyme.System) (M : Enzyme.Mappable) = struct
-  include S 
-
-  let map = M.map
-  type 'a wrapper = 'a M.t
-  type _ action = 
-  | D: dna_bases wrapper -> dna_bases action 
-  | R: rna_bases wrapper -> rna_bases action
-  
-  let to_baseD strand = M.map (function 
-  | #dna_bases as b -> Some b
-  | _ -> None) strand
-
-  let to_baseR strand = M.map (function
-  | #rna_bases as b -> Some b
-  | _ -> None) strand
-
-  let replicate : type a . a action -> a action = function
-  | D base -> D (map complementD base)
-  | R base -> R (map complementR base)
-
-  let transcribe = function
-  | D base -> R (map complementR base)
-
-  let reverse_transcribe = function
-  | R base -> D (map complementD base)
-
-  let interpret : type a . a action -> a action = function
-  | D _ as b -> b | R _ as b -> b 
-
-  let unwrap : type a . a action -> a wrapper = function
-  | D b -> b | R b -> b 
-
-  let to_strings : type a . a action -> string wrapper = function
-  | D bases -> map to_string bases
-  | R bases -> map to_string bases
+open Enzyme
+module Machinery (S : System) (M : Mappable) = struct
+  type dna = S.dna
+  type rna = S.rna
+  type bases = S.bases
+  type 'a t = 'a M.t
+  type 'a backbone = 'a S.backbone
+  let toDNA = M.map S.toDNA 
+  let toRNA = M.map S.toRNA
+  let replicate bases = M.map S.replicate bases
+  let transcribe = M.map S.transcribe
+  let reverse_transcribe = M.map S.reverse_transcribe
 end
-module StandardSystem : sig 
-  include Enzyme.System
-  type 'a wrapper = 'a option array 
-  type _ action = 
-  | D: dna_bases wrapper -> dna_bases action 
-  | R: rna_bases wrapper -> rna_bases action
-  val transcribe: dna_bases action -> rna_bases action
-  val reverse_transcribe: rna_bases action -> dna_bases action
-  val replicate: 'a action -> 'a action
-  val unwrap: 'a action -> 'a wrapper
-  val to_baseD: [> dna_bases] wrapper -> dna_bases option wrapper 
-  val to_baseR: [> rna_bases] wrapper -> rna_bases option wrapper
-  val to_strings: 'a action -> string wrapper
-  
-end = ConstructGrammar(Enzyme.Standard)(Options)
-open StandardSystem
-let mat = replicate (transcribe (replicate (D [| Some `T |])))
