833d8ec1e56fa8f90c99bc15322928fbc68ae3c5
spritefullake <42ahmad@gmail.com>
2020-05-09 13:51:38 -0400

    created synthesize functor and commented

--- src/Enzyme.ml
+++ src/Enzyme.ml
@@ -1,4 +1,7 @@
-
+(* 
+a genetics system with the given nucleic bases 
+for use in various genetic actions like replication
+*)
 module type System = sig 
   type dna = [`A | `T | `G | `C] 
   type rna = [`A | `U | `G | `C] 
@@ -9,11 +12,24 @@ module type System = sig
   | None  
   val toDNA: bases -> dna backbone 
   val toRNA: bases -> rna backbone
+end
+(* 
+  The main genetics machinery that allows life to replicate 
+  and for genes to turn into mRNA
+*)
+module type Machinery = sig 
+  include System
   val replicate: 'a backbone -> 'a backbone 
   val transcribe: dna backbone -> rna backbone 
   val reverse_transcribe: rna backbone -> dna backbone
   val to_string: 'a backbone -> string
+end 
+(* Meant for using generic containers containing bases with a genetic machinery *)
+module type Mappable = sig 
+  type 'a t 
+  val map: ('a -> 'b) -> 'a t -> 'b t
 end
+
 module Standard : System = struct
   type dna = [`A | `T | `G | `C] 
   type rna = [`A | `U | `G | `C] 
@@ -22,7 +38,34 @@ module Standard : System = struct
   | D: dna -> dna backbone 
   | R: rna -> rna backbone
   | None 
-
+  let toDNA = function #dna as base -> D base | _ -> None
+  let toRNA = function #rna as base -> R base | _ -> None
+end
+module Synthesize (S : sig 
+  include System 
+  val complementDNA: bases -> dna backbone 
+  val complementRNA: bases -> rna backbone
+  val base_to_string: [< dna | rna] -> string
+end) : Machinery
+= struct
+  include S
+  let to_string : type a . a backbone -> string = function 
+  | D base -> base_to_string base 
+  | R base -> base_to_string base 
+  | None -> ""  
+  let replicate : type a . a backbone -> a backbone = function 
+  | D base -> complementDNA (base :> bases) 
+  | R base -> complementRNA (base :> bases)
+  | None -> None
+  let transcribe = function
+  | D base -> complementRNA (base :> bases)
+  | None -> None
+  let reverse_transcribe = function 
+  | R base -> complementDNA (base :> bases) 
+  | None -> None 
+end
+module Enzyme : Machinery = Synthesize(struct 
+  include Standard
   let complementDNA = function 
   | `A -> D `T | `T -> D `A 
   | `G -> D `C | `C -> D `G 
@@ -35,25 +78,4 @@ module Standard : System = struct
   | _ -> None
   let base_to_string = function 
   | `A -> "A" | `T -> "T" | `G -> "G" | `C -> "C" | `U -> "U"
-  let to_string : type a . a backbone -> string = function 
-  | D base -> base_to_string base 
-  | R base -> base_to_string base 
-  | None -> ""
-  let toDNA = function #dna as base -> D base | _ -> None
-  let toRNA = function #rna as base -> R base | _ -> None
-  
-  let replicate : type a . a backbone -> a backbone = function 
-  | D base -> complementDNA (base :> bases) 
-  | R base -> complementRNA (base :> bases)
-  | None -> None
-  let transcribe = function
-  | D base -> complementRNA (base :> bases)
-  | None -> None
-  let reverse_transcribe = function 
-  | R base -> complementDNA (base :> bases) 
-  | None -> None 
-end
-module type Mappable = sig 
-  type 'a t 
-  val map: ('a -> 'b) -> 'a t -> 'b t
-end
+end)
--- src/Polymerase.ml
+++ src/Polymerase.ml
@@ -1,14 +1,10 @@
-module Options : Enzyme.Mappable with type 'a t = 'a option array = struct
+open Enzyme
+module Options : Mappable with type 'a t = 'a option array = struct
   type 'a t = 'a option array 
   let map f = Array.map (function | Some x -> Some (f x) | None -> None)
 end
-open Enzyme
-module Machinery (S : System) (M : Mappable) = struct
-  type dna = S.dna
-  type rna = S.rna
-  type bases = S.bases
-  type 'a t = 'a M.t
-  type 'a backbone = 'a S.backbone
+module Machinery (S : Machinery) (M : Mappable) = struct
+  include S
   let toDNA = M.map S.toDNA 
   let toRNA = M.map S.toRNA
   let replicate bases = M.map S.replicate bases
@@ -17,7 +13,7 @@ module Machinery (S : System) (M : Mappable) = struct
   let to_string bases = M.map S.to_string bases
 end
 
-module Poly = Machinery(Standard)(Options)
+module Poly = Machinery(Enzyme)(Options)
 
 let sham : Poly.bases option array = [| Some `A; Some `C; Some `T; Some `U; None; None |]
 let ram = sham |> Array.fold_left (fun acc x -> x |> function | Some x -> Array.append acc [|x|] | None -> acc) [||]
