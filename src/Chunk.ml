2508f97f73cff76c1d6429dee00472a215bcdc56
spritefullake <42ahmad@gmail.com>
2020-05-16 01:13:24 -0400

    remove extraneous functions

--- src/Chunk.ml
+++ src/Chunk.ml
@@ -46,23 +46,3 @@ let drop size input =
     | _, 0 -> input 
     | _head::tail, count -> loop tail (count - 1) in 
   loop input size
-
-let chunk_list size input =
-  let rec loop acc = function
-  | list when (List.length list <= size) -> list::acc
-  | list -> 
-      let elem = take size list in 
-      loop (elem::acc) (drop size list)
-  in loop [] input |> List.rev
-
-let chunk_array size input =
-  let length = Array.length input in
-  let bins = length / size + (length mod size) in
-  let result = Array.make_matrix (Array.length input / size) size input.(0) in
-  for i = 0 to bins do
-    if i + size <= Array.length input then
-      result.(i) <- (Array.sub input i size)
-    else
-      result.(i) <- (Array.sub input i (Array.length input))
-  done;
-  result
